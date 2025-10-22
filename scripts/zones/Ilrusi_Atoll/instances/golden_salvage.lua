46fdeed2cf2702671def17d345712a9a9c34a328
Jinorz <kc.cotton@windowslive.com>
2025-10-23 02:54:43 +1300

    Cleaning up prints

--- scripts/zones/Ilrusi_Atoll/instances/golden_salvage.lua
+++ scripts/zones/Ilrusi_Atoll/instances/golden_salvage.lua
@@ -1,6 +1,8 @@
 -----------------------------------
 -- Assault: Golden Salvage
--- TODO: random the chest locations
+-- TODO:    chests on boats can spawn in at atleast 4 random spots on each boat
+--          test party play for bugs (I have not done this)
+--          
 -----------------------------------
 require("scripts/globals/instance")
 require("scripts/globals/assault")
@@ -9,13 +11,6 @@ local ID = require("scripts/zones/Ilrusi_Atoll/IDs")
 local instanceObject = {}
 
 instanceObject.registryRequirements = function(player)
-    print("[GS] registryRequirements called for", player:getName())
-    print("[GS] registry: hasOrders=", player:hasKeyItem(xi.ki.ILRUSI_ASSAULT_ORDERS),
-        " currentAssault=", player:getCurrentAssault(),
-        " entered=", player:getCharVar("assaultEntered"),
-        " hasArmband=", player:hasKeyItem(xi.ki.ASSAULT_ARMBAND),
-        " level=", player:getMainLvl())
-
     return player:hasKeyItem(xi.ki.ILRUSI_ASSAULT_ORDERS) and
         player:getCurrentAssault() == xi.assault.mission.GOLDEN_SALVAGE and
         player:getCharVar("assaultEntered") == 0 and
@@ -24,12 +19,6 @@ instanceObject.registryRequirements = function(player)
 end
 
 instanceObject.entryRequirements = function(player)
-    print("[GS] entryRequirements called for", player:getName())
-    print("[GS] entry: hasOrders=", player:hasKeyItem(xi.ki.ILRUSI_ASSAULT_ORDERS),
-        " currentAssault=", player:getCurrentAssault(),
-        " entered=", player:getCharVar("assaultEntered"),
-        " level=", player:getMainLvl())
-
     return player:hasKeyItem(xi.ki.ILRUSI_ASSAULT_ORDERS) and
         player:getCurrentAssault() == xi.assault.mission.GOLDEN_SALVAGE and
         player:getCharVar("assaultEntered") == 0 and
@@ -37,43 +26,7 @@ instanceObject.entryRequirements = function(player)
 end
 
 instanceObject.afterInstanceRegister = function(player)
-    local instance = player:getInstance()
-    print("randomly spawning chests")
-
-    
-    --[[
-    local spawnPoints =
-        {
-            [1]  = {590,-15, 109,127},
-            [2]  = {346, -2, 113, 49},
-            [3]  = {351,-15, -14,134},
-            [4]  = {288,-15,-105,248},
-            [5]  = {331,-15,-181,202},
-            [6]  = {330, -3, -34,163},
-            [7]  = {221, -1, -32,226},
-            [8]  = {546, -7, 161,156},
-            [9]  = {334,-15,-145,132},
-            [10] = {370,-16,-131, 75},
-            [11] = {305, -2,  73, 54},
-            [12] = {273, -2,  30, 99},
-            [13] = {380, -2, 149, 78},
-            [14] = {473, -2, 133,131},
-            [15] = {462, -2, 181,130},
-            [16] = {546, -8, 258, 81},
-        }
-
-    for i = ID.mob[GOLDEN_SALVAGE].ILRUSI_CURSED_CHEST_OFFSET, ID.mob[GOLDEN_SALVAGE].ILRUSI_CURSED_CHEST_OFFSET + 7 do
-        local sPoint = math.random(1,#spawnPoints) -- Randoms the 1st 7 points for chests, last 4 are static on boats
-        GetMobByID(i, instance):setSpawn(spawnPoints[sPoint])
-        SpawnMob(i, instance)
-        table.remove(spawnPoints,sPoint)
-    end
-    --]]
-    
-    
-
-
-    
+    local instance = player:getInstance()  
     xi.assault.afterInstanceRegister(player, xi.items.CAGE_OF_REEF_FIREFLIES)
 end
 
@@ -113,14 +66,14 @@ instanceObject.onInstanceCreated = function(instance)
         return false
     end
 
-    -- Spawn all chests first
+    -- spawn all chests first
     local allChestIDs = {}
     for i = ID.npc.ILRUSI_CURSED_CHEST_OFFSET, ID.npc.ILRUSI_CURSED_CHEST_OFFSET + 11 do
         table.insert(allChestIDs, i)
         SpawnMob(i, instance)
     end
 
-    -- Create a list of the remaining chest IDs (excluding boat chests)
+    -- create a list of the remaining chest IDs (excluding boat chests)
     local boatChestIDs = {17002505, 17002509, 17002512, 17002514}
     local remainingChestIDs = {}
     for _, chestID in ipairs(allChestIDs) do
@@ -129,32 +82,28 @@ instanceObject.onInstanceCreated = function(instance)
         end
     end
 
-    -- Randomly assign the remaining chests to the spawn points
+    -- randomly assign the remaining chests to the spawn points
     for _, chestID in ipairs(remainingChestIDs) do
         if #spawnPoints > 0 then
             local randomIndex = math.random(1, #spawnPoints)
             local coords = spawnPoints[randomIndex]
-            table.remove(spawnPoints, randomIndex) -- Remove the used spawn point
+            table.remove(spawnPoints, randomIndex) -- remove the used spawn point
 
-            -- Position the mob and its associated NPC
+            -- position the mob and its associated NPC
             local mob = GetMobByID(chestID, instance)
             local npc = GetNPCByID(chestID, instance)
             if mob and npc then
-                mob:setPos(coords[1], coords[2], coords[3], coords[4]) -- Set mob position
-                npc:setPos(coords[1], coords[2], coords[3], coords[4]) -- Set NPC position
+                mob:setPos(coords[1], coords[2], coords[3], coords[4]) -- set mob position
+                npc:setPos(coords[1], coords[2], coords[3], coords[4]) -- set NPC position
             end 
         end
     end
 
-    
-
-    print("[GS] placing Rune of Release and Ancient Lockbox")
-    GetNPCByID(ID.npc.RUNE_OF_RELEASE, instance):setPos(420, -15, 70, 148)
-    GetNPCByID(ID.npc.ANCIENT_LOCKBOX, instance):setPos(415, -15, 75, 148)
+    GetNPCByID(ID.npc.RUNE_OF_RELEASE, instance):setPos(380, -7.885, 65, 148)
+    GetNPCByID(ID.npc.ANCIENT_LOCKBOX, instance):setPos(380, -7.752, 62, 148)
     GetNPCByID(ID.npc._1jp, instance):setAnimation(8)
     GetNPCByID(ID.npc._jja, instance):setAnimation(8)
     GetNPCByID(ID.npc._jjb, instance):setAnimation(8)
-
     instance:setProgress(0)
 
 end
@@ -174,14 +123,12 @@ instanceObject.onInstanceFailure = function(instance)
 end
 
 instanceObject.onInstanceProgressUpdate = function(instance, progress)
-    --TODO: if progress == 1 then instance:complete(). use this instead of manual vars
     if instance:getProgress() >= 1 then
         instance:complete()
     end
 end
 
 instanceObject.onInstanceComplete = function(instance)
-    -- Determine rune position from the instance's Rune of Release NPC and forward it
     local runeNpc = GetNPCByID(ID.npc.RUNE_OF_RELEASE, instance)
     local posX, posZ = 7, 7
 
@@ -199,13 +146,10 @@ instanceObject.onInstanceComplete = function(instance)
 end
 
 instanceObject.onEventUpdate = function(player, csid, option)
-    print("[GS] onEventUpdate csid=", csid, " option=", option)
 end
 
 instanceObject.onEventFinish = function(player, csid, option)
     xi.assault.instanceOnEventFinish(player, csid, xi.zone.ILRUSI_ATOLL)
 end
-
-
 
 return instanceObject
--- scripts/zones/Ilrusi_Atoll/mobs/Cursed_Chest.lua
+++ scripts/zones/Ilrusi_Atoll/mobs/Cursed_Chest.lua
@@ -1,25 +1,19 @@
 -----------------------------------
 -- Area: Ilrusi Atoll
 --  Mob: Cursed Chest
+-- TODO: 
 -----------------------------------
 local ID = require("scripts/zones/Ilrusi_Atoll/IDs")
-local goldenSalvage = require("scripts/zones/Ilrusi_Atoll/instances/golden_salvage")
 local entity = {}
-print("[GS][Chest][LOAD] mob Cursed_Chest.lua loaded")
 
 entity.onTrigger = function(player, mob)
     local instance = player:getInstance()
 
-    print("ontrigger triggered in mobs/cursed_chest.lua")
-
     if instance:getLocalVar("figureheadChestOpened") == 1 then
-        print("[GS][Chest][onTrigger] already opened -> EXIT")
         return
     end
 
-
     if player:checkDistance(mob) > 3 then
-        print("[GS][Chest][onTrigger] too far -> MUST_BE_CLOSER_CHEST")
         player:messageSpecial(ID.text.MUST_BE_CLOSER_CHEST)
         return
     end
@@ -33,38 +27,23 @@ entity.onTrigger = function(player, mob)
         instance:setLocalVar("figureheadChestOpened", 1)
         print("[GS][Chest][onTrigger] MATCH -> GOLDEN")
         print(instance:getLocalVar("figureheadChestOpened"))
-       
-        --TODO: a player an ctrl+a the winningchest and kill it, prevent that or make a if else statement
-        --despawn the chests after winning
-        --cursed chest doesnt aggro a lvl 99 whereas on retail it does. it would be easy to make it aggro but keeping the name yellow(unclaimed) while doing so might be harder
-        --check if the portal and lockbox spawns and you can tele out with awards
-
-     
 
         mob:entityAnimationPacket("open")
         player:messageSpecial(ID.text.CHEST)
-
 
         player:timer(3000, function()
         player:messageSpecial(ID.text.GOLDEN)
         end)
 
         player:timer(20000, function()
-        instance:setProgress(1)
-        
-        
+        instance:setProgress(1)        
         end)
-        print("ending mob onTrigger")
-
     else
-        print("[GS][Chest][onTrigger] mob:ontrigger NO MATCH")
         mob:setModelId(258)
         mob:setAnimationSub(1)
         mob:setStatus(xi.status.UPDATE)
         mob:setMobMod(xi.mobMod.NO_AGGRO, 0)
     end
-    
-    print("ending mob onTrigger")
 end
 
  
@@ -73,27 +52,21 @@ entity.onMobSpawn = function(mob)
     local mobID    = mob:getID()
     local instance = mob:getInstance()
     local figureheadChestID = instance and instance:getLocalVar("figureheadChestID")
-    local randomRotation = math.random() * 2 * math.pi
-    mob:setRotation(randomRotation)
+    local randomRotation = math.random(0, 255)
 
-    
-    --mob:hideName(false) 
+    mob:setRotation(randomRotation)
     mob:setStatus(xi.status.NORMAL)
     mob:setAnimationSub(0)
     mob:setMobMod(xi.mobMod.NO_DESPAWN, 1)
     mob:setMobMod(xi.mobMod.NO_AGGRO, 1)
     DisallowRespawn(mob:getID(), true)
-        
-
-    print("[GS][Mob][onMobSpawn] finished init; status=NORMAL hidden=true animSub=0")
 end
 
 entity.onMobEngaged = function(mob, target)
     mob:setStatus(1)
     mob:hideName(false)
     mob:setModelId(258)
-    mob:setAnimationSub(0)
-    
+    mob:setAnimationSub(0)   
 end
 
 entity.onMobDisengage = function(mob)
@@ -101,9 +74,7 @@ entity.onMobDisengage = function(mob)
     mob:setAnimationSub(0)
     mob:setModelId(960)
     mob:hideName(true)
-    mob:setMobMod(xi.mobMod.NO_AGGRO, 1)
-    
-    
+    mob:setMobMod(xi.mobMod.NO_AGGRO, 1)  
 end
 
 entity.onMobFight = function(mob, target)
@@ -127,12 +98,10 @@ entity.onMobFight = function(mob, target)
 end
 
 entity.onMobDeath = function(mob, player, optParams)
-    print("[GS][Mob][onMobDeath] mob=", mob:getID(), " killer=", player and player:getName() or "nil") 
 end
 
 entity.onMobDespawn = function(mob)
-    GetNPCByID(mob:getID(), mob:getInstance()):setStatus(xi.status.DISAPPEAR)
-    
+    GetNPCByID(mob:getID(), mob:getInstance()):setStatus(xi.status.DISAPPEAR) 
 end
 
 return entity
